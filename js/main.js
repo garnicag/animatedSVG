4aaa467d84afe2bd5f86138833c2843eae636095
Gabriel Garnica <brian.garnica@prodigious.com>
2016-04-09 10:27:08 -0500

    Syntax improvements

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
-var animationSpeed = 80;
+var animationSpeed = 8;
 
 setInterval(function() {
     var container = $('.animatedBkg svg'),
         items = container.find('polyline'),
-        numberOfItems = items.length,
-        randomSel,
-        randomColor;
+        numberOfItems = items.length;
 
     function randomSel() {
         return Math.floor(Math.random() * numberOfItems);
     };
 
-    randomColor = function() {
+    function randomColor() {
     	return '#' + Math.floor(Math.random() * 16777215).toString(16);
     };
 
-    items.eq(randomSel()).attr('class', 'animFadeIn').attr('fill', randomColor);
+    console.log(randomColor());
+
+    items.eq(randomSel()).attr('class', 'animFadeIn').attr('fill', randomColor());
     items.eq(randomSel()).attr('class', 'animFadeOut');
+
 }, animationSpeed);
